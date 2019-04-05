6020cc4db679be48b086e80d38e7cdca8ae7b4fc
kreestoff <christopher.l.mcintosh@outlook.com>
2019-04-05 08:49:29 -0500

    solved

---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+require_relative 'artist.rb'
+require_relative 'song.rb'
+
+class Genre
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select {|songs| songs.genre == self}
+  end
+
+  def artists
+    self.songs.map {|songs| songs.artist}
+  end
+
+
+end
